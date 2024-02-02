89ed4a7b625633c3bbecfe4fb6934335e2a5c8fd
ArgentiTrades <158603013+ArgentiTrades@users.noreply.github.com>
2024-02-02 10:07:08 -0700

    Add files via upload

---
+++ trades.js
@@ -0,0 +1,133 @@
+var trades = [
+    [
+        {
+            'job': 'librarian', 
+            // 'filename': 'LOOLA Q45XW0PT.png', 
+            'numtrades': 9, 
+            'listtrades': [
+                [ ['paper', 24], ['emerald', 1] ], 
+                [ ['emerald', 9], ['bookshelf', 1] ], 
+                [ ['book', 4], ['emerald', 1] ], 
+                [ ['emerald-book', '36-1'], ['enchanted book/sharpness 3', 1] ], 
+                [ ['ink sac', 36], ['emerald', 1] ],
+                [ ['emerald-book', '19-1'], ['enchanted book/protection 2', 1] ], 
+                [ ['book n quill', 2], ['emerald', 1] ],
+                [ ['emerald-book', '15-1'], ['enchanted book/looting 2', 1] ],
+                [ ['emerald', 20], ['name tag', 1] ],
+            ]
+        },
+        {
+            'job': 'librarian', 
+            // 'filename': 'LOOLA Q45XW0PT.png', 
+            'numtrades': 9, 
+            'listtrades': [
+                [ ['paper', 24], ['emerald', 1] ], 
+                [ ['emerald-book', '14-1'], ['enchanted book/flame 1', 1] ],  
+                [ ['book', 4], ['emerald', 1] ], 
+                [ ['emerald-book', '20-1'], ['enchanted book/mending', 1] ], 
+                [ ['ink sac', 36], ['emerald', 1] ],
+                [ ['emerald', 1], ['glass', 4] ], 
+                [ ['book n quill', 2], ['emerald', 1] ],
+                [ ['emerald-book', '7-1'], ['enchanted book/lure 1', 1] ],
+                [ ['emerald', 20], ['name tag', 1] ],
+            ]
+        },
+        {
+            'job': 'weapon smith', 
+            // 'filename': 'LOOLA Q45XW0PT.png', 
+            'numtrades': 9, 
+            'listtrades': [
+                [ ['coal', 15], ['emerald', 1] ], 
+                [ ['emerald', 3], ['iron axe', 1] ], 
+                [ ['emerald', 16], ['enchated iron sword', 1] ], 
+                [ ['iron', 4], ['emerald', 1] ], 
+                [ ['emerald', 36], ['bell', 1] ],
+                [ ['flint', 24], ['emerald', 1] ], 
+                [ ['diamond', 1], ['emerald', 1] ],
+                [ ['emerald', 30], ['enchanted diamond axe', 1] ],
+                [ ['emerald', 22], ['enchanted diamond sword', 1] ],
+            ]
+        },
+        {
+            'job': 'armorer', 
+            // 'filename': 'LOOLA Q45XW0PT.png', 
+            'numtrades': 9, 
+            'listtrades': [
+                [ ['coal', 15], ['emerald', 1] ], 
+                [ ['emerald', 5], ['iron helmet', 1] ], 
+                [ ['iron', 4], ['emerald', 1] ], 
+                [ ['emerald', 1], ['chainmail boots', 1] ],
+                [ ['lava bucket', 1], ['emerald', 1] ], 
+                [ ['diamond', 1], ['emerald', 1] ],
+                [ ['emerald', 4], ['chainmail chestplate', 1] ],
+                [ ['emerald', 26], ['enchanted diamond pants', 1] ],
+                [ ['emerald', 26], ['enchanted diamond helmet', 1] ],
+            ]
+        },
+        {
+            'job': 'farmer', 
+            // 'filename': 'LOOLA Q45XW0PT.png', 
+            'numtrades': 9, 
+            'listtrades': [
+                [ ['potato', 26], ['emerald', 1] ], 
+                [ ['emerald', 1], ['bread', 6] ], 
+                [ ['pumpkin', 6], ['emerald', 1] ], 
+                [ ['emerald', 1], ['pumpkin pie', 4] ],
+                [ ['melon', 4], ['emerald', 1] ], 
+                [ ['emerald', 3], ['cookie', 18] ],
+                [ ['emerald', 1], ['sus soup', 1] ],
+                [ ['emerald', 1], ['cake', 1] ],
+                [ ['emerald', 3], ['golden carrots', 3] ],
+            ]
+        },
+        {
+            'job': 'cartographer', 
+            // 'filename': 'LOOLA Q45XW0PT.png', 
+            'numtrades': 9, 
+            'listtrades': [
+                [ ['paper', 24], ['emerald', 1] ], 
+                [ ['emerald', 7], ['map', 1] ],  
+                [ ['glass pane', 4], ['emerald', 1] ], 
+                [ ['emerald-compass', '13-1'], ['woodland map', 1] ], 
+                [ ['compass', 1], ['emerald', 1] ],
+                [ ['emerald-compass', '14-1'], ['ocean map', 1] ],  
+                [ ['emerald', 7], ['item frame', 1] ],
+                [ ['emerald', 3], ['black banner', 1] ],
+                [ ['emerald', 8], ['globe banner design', 1] ],
+            ]
+        },
+        {
+            'job': 'armorer', 
+            // 'filename': 'LOOLA Q45XW0PT.png', 
+            'numtrades': 9, 
+            'listtrades': [
+                [ ['coal', 15], ['emerald', 1] ], 
+                [ ['emerald', 4], ['iron boots', 1] ], 
+                [ ['iron', 4], ['emerald', 1] ], 
+                [ ['emerald', 36], ['bell', 1] ],
+                [ ['lava bucket', 1], ['emerald', 1] ], 
+                [ ['diamond', 1], ['emerald', 1] ],
+                [ ['emerald', 4], ['chainmail chestplate', 1] ],
+                [ ['emerald', 18], ['enchanted diamond boots', 1] ],
+                [ ['emerald', 27], ['enchanted diamond chestplate', 1] ],
+            ]
+        },
+        {
+            'job': 'fletcher', 
+            // 'filename': 'LOOLA Q45XW0PT.png', 
+            'numtrades': 10, 
+            'listtrades': [
+                [ ['stick', 32], ['emerald', 1] ], 
+                [ ['emerald', 1], ['arrow', 16] ], 
+                [ ['flint', 26], ['emerald', 1] ], 
+                [ ['emerald', 2], ['bow', 1] ],
+                [ ['string', 14], ['emerald', 1] ], 
+                [ ['emerald', 3], ['crossbow', 1] ],
+                [ ['feather', 24], ['emerald', 1] ],
+                [ ['emerald', 15], ['enchanted bow', 1] ],
+                [ ['tripwire hook', 8], ['emerald', 1] ],
+                [ ['emerald', 11], ['enchanted crossbow', 1] ],
+            ]
+        },
+    ]
+]
